7af96663faa686b47cb57088b5b0ef46009adf25
RoshanShaikh <12roshanshaikh@gmail.com>
2022-05-05 17:32:58 +0530

    .l added in lexical analyzer

--- Lexical Analizer/lexer.c
+++ Lexical Analizer/lexer.c
@@ -1,39 +1,43 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<string.h>
-char keyword[6][10] = {"ANSWER", "ASSIGN", "RESULT", "DECLARE", "#START",
-"#END"};
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+char keyword[6][10] = {"ANSWER", "ASSIGN", "RESULT", "DECLARE", "#START", "#END"};
 char operation[4][5] = {"ADD", "SUB", "DIV", "MUL"};
-int isKeyword(char *str){
-for(int i=0; i<6; i++)
-if(!strcmp(str, keyword[i]))
-return 1;
-return 0;
+int isKeyword(char *str)
+{
+    for (int i = 0; i < 6; i++)
+        if (!strcmp(str, keyword[i]))
+            return 1;
+    return 0;
 }
-int isOperation(char *str){
-for(int i=0; i<4; i++)
-if(!strcmp(str, operation[i]))
-return 1;
-return 0;
+int isOperation(char *str)
+{
+    for (int i = 0; i < 4; i++)
+        if (!strcmp(str, operation[i]))
+            return 1;
+    return 0;
 }
-int isNumber(char *str){
-for(int i=0; i<strlen(str); i++)
-if(!isdigit(str[i]) && str[i]!='.')
-return 0;
-return 1;
+int isNumber(char *str)
+{
+    for (int i = 0; i < strlen(str); i++)
+        if (!isdigit(str[i]) && str[i] != '.')
+            return 0;
+    return 1;
 }
-int isWord(char *str){
-for(int i=0; i<strlen(str); i++)
-if(!isupper(str[i]))
-return 0;
-return 1;
+int isWord(char *str)
+{
+    for (int i = 0; i < strlen(str); i++)
+        if (!isupper(str[i]))
+            return 0;
+    return 1;
 }
-int isID(char *str){
-if(strlen(str)==2)
-if(isupper(str[0]))
-    if (isdigit(str[1]))
-        return 1;
-return 0;
+int isID(char *str)
+{
+    if (strlen(str) == 2)
+        if (isupper(str[0]))
+            if (isdigit(str[1]))
+                return 1;
+    return 0;
 }
 int isComment(char *str)
 {
